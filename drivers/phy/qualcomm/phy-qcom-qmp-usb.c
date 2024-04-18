dea114ab943b7ab566ac4eb7884e58d3955efa7e
Sriram Dash <quic_sriramd@quicinc.com>
2024-04-18 10:42:10 +0530

    phy: qcom: qmp-usb: Add support for firmware managed resources
    QCOM SoC sa8775p allow multiple resources to be managed
    by firmware. On this platform, tasks related to clocks, regulators,
    resets, TLMM and interconnects can be delegated to the firmware,
    while the remaining responsibilities are handled by Linux.
    
    So, instead of using CCU, regulator framework, etc, the firmware
    will control the resources.
    
    The power domain, managed through the PM runtime framework,
    is utilized for activation/deactivation of the resources.
    
    The driver is responsible for managing multiple power domains and
    linking them to consumers as needed.
    
    Power domain "usb_core" will control the GDSC, resets,
    and regulators whereas power domain "usb_transfer"
    will control clocks.
    
    "qcom,firmware-managed-resources" property allows the driver to
    determine if device resources are managed by firmware.
    
    "dev_pm_domain_attach_by_name" finds the appropriate power doamin
    if available and associates it with a virtual device "core_pd" or
    "transfer_pd". These virtual devices can be controlled via generic
    apis like "pm_runtime_put_sync" and "pm_runtime_get_sync" and the
    firmware can take appropriate actions for the powrer domains.
    
    New api "qmp_usb_remove" is added to remove the virtual
    devices associated with PM domain.
    
    Change-Id: I5f755fd979af304874a5720b45d45920c48d5641
    Signed-off-by: Sriram Dash <quic_sriramd@quicinc.com>
    Signed-off-by: Shazad Hussain <quic_shazhuss@quicinc.com>

--- drivers/phy/qualcomm/phy-qcom-qmp-usb.c
+++ drivers/phy/qualcomm/phy-qcom-qmp-usb.c
@@ -14,6 +14,7 @@
 #include <linux/of.h>
 #include <linux/of_device.h>
 #include <linux/of_address.h>
+#include <linux/pm_domain.h>
 #include <linux/phy/phy.h>
 #include <linux/platform_device.h>
 #include <linux/regulator/consumer.h>
@@ -1133,6 +1134,12 @@ struct qmp_usb {
 	struct phy *phy;
 
 	struct clk_fixed_rate pipe_clk_fixed;
+
+	struct dev_pm_domain_list *pd_list;
+	struct device *core_pd;
+	struct device *transfer_pd;
+
+	bool fw_managed;
 };
 
 static inline void qphy_setbits(void __iomem *base, u32 offset, u32 val)
@@ -1459,6 +1466,35 @@ static void qmp_usb_configure(void __iomem *base,
 	qmp_usb_configure_lane(base, tbl, num, 0xff);
 }
 
+static void qmp_usb_fw_managed_domain_remove(struct qmp_usb *qmp)
+{
+	dev_pm_domain_detach_list(qmp->pd_list);
+}
+
+static int qmp_usb_fw_managed_domain_get(struct qmp_usb *qmp)
+{
+	struct device *dev = qmp->dev;
+	struct dev_pm_domain_attach_data pd_data = {
+		.pd_flags	= PD_FLAG_NO_DEV_LINK,
+		.pd_names	= (const char*[]) { "usb_transfer", "usb_core" },
+		.num_pd_names	= 2,
+	};
+	int ret = 0;
+
+	if (!dev->pm_domain) {
+		ret = dev_pm_domain_attach_list(dev, &pd_data, &qmp->pd_list);
+		if (ret < 0) {
+			dev_err(dev, "domain attach failed %d)\n", ret);
+			return ret;
+		}
+		qmp->transfer_pd = qmp->pd_list->pd_devs[0];
+		qmp->core_pd = qmp->pd_list->pd_devs[1];
+	} else
+		return -ENODEV;
+
+	return 0;
+}
+
 static int qmp_usb_serdes_init(struct qmp_usb *qmp)
 {
 	const struct qmp_phy_cfg *cfg = qmp->cfg;
@@ -1477,6 +1513,22 @@ static int qmp_usb_init(struct phy *phy)
 	const struct qmp_phy_cfg *cfg = qmp->cfg;
 	void __iomem *pcs = qmp->pcs;
 	int ret;
+
+	if (qmp->fw_managed) {
+		ret = pm_runtime_resume_and_get(qmp->core_pd);
+		if (ret < 0) {
+			dev_err(qmp->dev,
+				"Failed to enable fw managed resources");
+			return ret;
+		}
+
+		ret = pm_runtime_resume_and_get(qmp->transfer_pd);
+		if (ret < 0) {
+			dev_err(qmp->dev,
+				"Failed to enable fw managed resources");
+			goto err_disable_regulators;
+		}
+	}
 
 	ret = regulator_bulk_enable(cfg->num_vregs, qmp->vregs);
 	if (ret) {
@@ -1507,6 +1559,9 @@ static int qmp_usb_init(struct phy *phy)
 err_assert_reset:
 	reset_control_bulk_assert(qmp->num_resets, qmp->resets);
 err_disable_regulators:
+	if (qmp->fw_managed)
+		pm_runtime_put_sync(qmp->core_pd);
+
 	regulator_bulk_disable(cfg->num_vregs, qmp->vregs);
 
 	return ret;
@@ -1516,6 +1571,19 @@ static int qmp_usb_exit(struct phy *phy)
 {
 	struct qmp_usb *qmp = phy_get_drvdata(phy);
 	const struct qmp_phy_cfg *cfg = qmp->cfg;
+	int ret = 0;
+
+	if (qmp->fw_managed) {
+		ret = pm_runtime_put_sync(qmp->transfer_pd);
+		if (ret < 0)
+			dev_err(qmp->dev,
+				"Failed to disable fw managed resources");
+
+		ret = pm_runtime_put_sync(qmp->core_pd);
+		if (ret < 0)
+			dev_err(qmp->dev,
+				"Failed to disable fw managed resources");
+	}
 
 	reset_control_bulk_assert(qmp->num_resets, qmp->resets);
 
@@ -1697,6 +1765,7 @@ static void qmp_usb_disable_autonomous_mode(struct qmp_usb *qmp)
 static int __maybe_unused qmp_usb_runtime_suspend(struct device *dev)
 {
 	struct qmp_usb *qmp = dev_get_drvdata(dev);
+	int ret;
 
 	dev_vdbg(dev, "Suspending QMP phy, mode:%d\n", qmp->mode);
 
@@ -1706,6 +1775,13 @@ static int __maybe_unused qmp_usb_runtime_suspend(struct device *dev)
 	}
 
 	qmp_usb_enable_autonomous_mode(qmp);
+
+	if (qmp->fw_managed) {
+		ret = pm_runtime_put_sync(qmp->transfer_pd);
+		if (ret < 0)
+			dev_err(qmp->dev,
+				"Failed to disable fw managed resources");
+	}
 
 	clk_disable_unprepare(qmp->pipe_clk);
 	clk_bulk_disable_unprepare(qmp->num_clks, qmp->clks);
@@ -1723,6 +1799,15 @@ static int __maybe_unused qmp_usb_runtime_resume(struct device *dev)
 	if (!qmp->phy->init_count) {
 		dev_vdbg(dev, "PHY not initialized, bailing out\n");
 		return 0;
+	}
+
+	if (qmp->fw_managed) {
+		ret = pm_runtime_resume_and_get(qmp->transfer_pd);
+		if (ret < 0) {
+			dev_err(qmp->dev,
+				"Failed to enable fw managed resources");
+			return ret;
+		}
 	}
 
 	ret = clk_bulk_prepare_enable(qmp->num_clks, qmp->clks);
@@ -1937,10 +2022,12 @@ static int qmp_usb_parse_dt_legacy(struct qmp_usb *qmp, struct device_node *np)
 		qmp->pcs_misc = NULL;
 	}
 
-	qmp->pipe_clk = devm_get_clk_from_child(dev, np, NULL);
-	if (IS_ERR(qmp->pipe_clk)) {
-		return dev_err_probe(dev, PTR_ERR(qmp->pipe_clk),
-				     "failed to get pipe clock\n");
+	if (!qmp->fw_managed) {
+		qmp->pipe_clk = devm_get_clk_from_child(dev, np, NULL);
+		if (IS_ERR(qmp->pipe_clk)) {
+			return dev_err_probe(dev, PTR_ERR(qmp->pipe_clk),
+					     "failed to get pipe clock\n");
+		}
 	}
 
 	ret = devm_clk_bulk_get_all(qmp->dev, &qmp->clks);
@@ -1949,10 +2036,12 @@ static int qmp_usb_parse_dt_legacy(struct qmp_usb *qmp, struct device_node *np)
 
 	qmp->num_clks = ret;
 
-	ret = qmp_usb_reset_init(qmp, usb3phy_legacy_reset_l,
-				 ARRAY_SIZE(usb3phy_legacy_reset_l));
-	if (ret)
-		return ret;
+	if (!qmp->fw_managed) {
+		ret = qmp_usb_reset_init(qmp, usb3phy_legacy_reset_l,
+					 ARRAY_SIZE(usb3phy_legacy_reset_l));
+		if (ret)
+			return ret;
+	}
 
 	return 0;
 }
@@ -1983,16 +2072,18 @@ static int qmp_usb_parse_dt(struct qmp_usb *qmp)
 	if (ret)
 		return ret;
 
-	qmp->pipe_clk = devm_clk_get(dev, "pipe");
-	if (IS_ERR(qmp->pipe_clk)) {
-		return dev_err_probe(dev, PTR_ERR(qmp->pipe_clk),
-				     "failed to get pipe clock\n");
-	}
+	if (!qmp->fw_managed) {
+		qmp->pipe_clk = devm_clk_get(dev, "pipe");
+		if (IS_ERR(qmp->pipe_clk)) {
+			return dev_err_probe(dev, PTR_ERR(qmp->pipe_clk),
+					     "failed to get pipe clock\n");
+		}
 
-	ret = qmp_usb_reset_init(qmp, usb3phy_reset_l,
-				 ARRAY_SIZE(usb3phy_reset_l));
-	if (ret)
-		return ret;
+		ret = qmp_usb_reset_init(qmp, usb3phy_reset_l,
+					 ARRAY_SIZE(usb3phy_reset_l));
+		if (ret)
+			return ret;
+	}
 
 	return 0;
 }
@@ -2014,6 +2105,16 @@ static int qmp_usb_probe(struct platform_device *pdev)
 	qmp->cfg = of_device_get_match_data(dev);
 	if (!qmp->cfg)
 		return -EINVAL;
+
+	qmp->fw_managed  = device_property_read_bool
+				(dev, "qcom,firmware-managed-resources");
+	if (qmp->fw_managed) {
+		ret = qmp_usb_fw_managed_domain_get(qmp);
+		if (ret) {
+			dev_err(dev, "Failed to init domains. Bail out");
+			return ret;
+		}
+	}
 
 	ret = qmp_usb_vreg_init(qmp);
 	if (ret)
@@ -2040,9 +2141,11 @@ static int qmp_usb_probe(struct platform_device *pdev)
 	 */
 	pm_runtime_forbid(dev);
 
-	ret = phy_pipe_clk_register(qmp, np);
-	if (ret)
-		goto err_node_put;
+	if (!qmp->fw_managed) {
+		ret = phy_pipe_clk_register(qmp, np);
+		if (ret)
+			goto err_node_put;
+	}
 
 	qmp->phy = devm_phy_create(dev, np, &qmp_usb_phy_ops);
 	if (IS_ERR(qmp->phy)) {
@@ -2061,7 +2164,20 @@ static int qmp_usb_probe(struct platform_device *pdev)
 
 err_node_put:
 	of_node_put(np);
+	if (qmp->fw_managed)
+		qmp_usb_fw_managed_domain_remove(qmp);
 	return ret;
+}
+
+static void qmp_usb_remove(struct platform_device *pdev)
+{
+	struct qmp_usb *qmp = platform_get_drvdata(pdev);
+
+	if (qmp->fw_managed) {
+		pm_runtime_put_sync(qmp->transfer_pd);
+		pm_runtime_put_sync(qmp->core_pd);
+		qmp_usb_fw_managed_domain_remove(qmp);
+	}
 }
 
 static const struct of_device_id qmp_usb_of_match_table[] = {
@@ -2117,6 +2233,7 @@ MODULE_DEVICE_TABLE(of, qmp_usb_of_match_table);
 
 static struct platform_driver qmp_usb_driver = {
 	.probe		= qmp_usb_probe,
+	.remove_new	= qmp_usb_remove,
 	.driver = {
 		.name	= "qcom-qmp-usb-phy",
 		.pm	= &qmp_usb_pm_ops,
