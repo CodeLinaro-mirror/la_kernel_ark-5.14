241e5debcd193fa7ab0decd9451d0e77be282068
Shazad Hussain <quic_shazhuss@quicinc.com>
2024-07-16 10:39:31 +0530

    Revert "opp: make stubbed out version of API as static"
    This reverts commit 1714ee6527e5dd1533600a3eb5e835e9fcc1332b.
    
    Change-Id: Ie4bf98d86ee74d878260a2dac738438f3f4804c3
    Signed-off-by: Shazad Hussain <quic_shazhuss@quicinc.com>

--- include/linux/pm_opp.h
+++ include/linux/pm_opp.h
@@ -436,7 +436,7 @@ static inline int dev_pm_opp_sync_regulators(struct device *dev)
 	return -EOPNOTSUPP;
 }
 
-static inline int dev_pm_opp_set_level(struct device *dev, unsigned int level)
+int dev_pm_opp_set_level(struct device *dev, unsigned int level)
 {
 	return -EOPNOTSUPP;
 }
